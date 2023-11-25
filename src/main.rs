2779aa9cb6cf5d5b45108fe881ec7dc22f4e7005
Isaac Turner <spacey_sooty@outlook.com>
2023-11-25 12:25:04 +0800

    parser stuff

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,8 @@ mod parser;
 fn main() {
     let input = "1 + 2 * 3 - 4 / 5 % 6";
     let tokens = lexer::lex_input(input.to_string());
-    println!("{:?}", tokens);
+    println!("{:?}\n", tokens);
+
+    let ast = parser::parse_lexed_input(tokens);
+    ast.traverse();
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,38 +1,84 @@
 use crate::lexer;
 
+#[derive(Clone, Debug)]
 pub struct Node {
     token: lexer::Token,
+    child: Option<lexer::Token>,
 }
 
+#[derive(Debug)]
 pub struct AST {
     pub root: Node,
-    pub stored: Vec<Node>
+    pub stored: Vec<Node>,
+}
+
+impl AST {
+    pub fn traverse(&self) {
+        for i in &self.stored {
+            println!("{:?} -> ", i);
+        }
+    }
 }
 
 pub fn parse_lexed_input(tokens: Vec<lexer::Token>) -> AST {
-    let mut iter_tokens = tokens.iter();
+    let mut iter_tokens = tokens.iter().peekable();
     let mut nodes: Vec<Node> = Vec::new();
-    let mut result: AST;
 
     while let Some(token) = iter_tokens.next() {
-        if tokens.first() == Some(token) {
-            result.root = token;
-        }
-
         match token {
             lexer::Token::Number(num) => {
                 nodes.push(Node {
                     token: lexer::Token::Number(*num),
+                    child: iter_tokens.peek().cloned().cloned(),
                 });
-            },
-            lexer::Token::Minus => {},
-            lexer::Token::Plus => {},
-            lexer::Token::Star => {},
-            lexer::Token::Slash => {},
-            lexer::Token::Modulo => {},
-            lexer::Token::LParen => {},
-            lexer::Token::RParen => {},
+            }
+            lexer::Token::Minus => {
+                nodes.push(Node {
+                    token: lexer::Token::Minus,
+                    child: iter_tokens.peek().cloned().cloned(),
+                });
+            }
+            lexer::Token::Plus => {
+                nodes.push(Node {
+                    token: lexer::Token::Plus,
+                    child: iter_tokens.peek().cloned().cloned(),
+                })
+            }
+            lexer::Token::Star => {
+                nodes.push(Node {
+                    token: lexer::Token::Star,
+                    child: iter_tokens.peek().cloned().cloned(),
+                });
+            }
+            lexer::Token::Slash => {
+                nodes.push(Node {
+                    token: lexer::Token::Slash,
+                    child: iter_tokens.peek().cloned().cloned(),
+                });
+            }
+            lexer::Token::Modulo => {
+                nodes.push(Node {
+                    token: lexer::Token::Modulo,
+                    child: iter_tokens.next().cloned(),
+                });
+            }
+            lexer::Token::LParen => {
+                nodes.push(Node {
+                    token: lexer::Token::LParen,
+                    child: iter_tokens.peek().cloned().cloned(),
+                });
+            }
+            lexer::Token::RParen => {
+                nodes.push(Node {
+                    token: lexer::Token::RParen,
+                    child: iter_tokens.peek().cloned().cloned(),
+                });
+            }
         }
     }
-    result
+
+    AST {
+        root: nodes.first().unwrap().clone(),
+        stored: nodes,
+    }
 }
